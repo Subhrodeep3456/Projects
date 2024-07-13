aebc12ad22840542c088f91c54e2c60c9b4998e9
cometcore <75169187+Subhrodeep3456@users.noreply.github.com>
2024-07-13 15:55:49 +0530

    Add files via upload

---
+++ Turtle Racing/race.py
@@ -0,0 +1,31 @@
+import turtle
+
+WIDTH, HEIGHT = 500, 500
+
+
+
+
+def get_number_of_racers():
+    racers = 0
+    while True:
+        racers = input("Enter no of racers (2-10): ")
+        if racers.isdigit():
+            racers = int(racers)
+        else:
+            print("Input invalid. Try Again!")
+            continue
+
+        if 2 <= racers <= 10:
+            return racers
+        else:
+            print("Number not in range 2-10. Try Again!")
+
+def init_turtle():
+    screen = turtle.Screen()
+    screen.setup(WIDTH, HEIGHT)
+    screen.title("Turtle Racing")
+
+racers = get_number_of_racers()
+init_turtle()
+
+
